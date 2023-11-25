46c8477a6d064d703e51cd79194df4d27505c89d
Dave <davearonmwan@gmail.com>
2023-11-25 10:16:39 +0100

    init sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import {
+	Hero,
+	Activities,
+	Founders,
+	History,
+	PastEvents,
+	Team,
+} from "./components";
+
 export default function App() {
-    return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
-    )
+	return (
+		<>
+			<Hero />
+			<Activities />
+			<Founders />
+			<History />
+			<PastEvents />
+			<Team />
+		</>
+	);
 }
---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Hero } from './Hero';
+export {default as Activities} from './Activities';
+export {default as Founders} from './Founders';
+export {default as History} from './History';
+export {default as PastEvents} from './PastEvents';
+export {default as Team} from './Team';
