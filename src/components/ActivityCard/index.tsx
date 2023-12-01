b7fd0936d1c77fa359596bc2bc1ffe65750b0b32
Dave <davearonmwan@gmail.com>
2023-12-01 21:32:32 +0100

    fixed merge conflicts

--- src/components/ActivityCard/index.tsx
+++ src/components/ActivityCard/index.tsx
@@ -1,14 +1,14 @@
 const index = () => {
 	return (
-		<article className="bg-clr-gdg-green-100 rounded-md overflow-hidden">
+		<article className="bg-clr-gdg-green-100/20 shadow-card rounded-md overflow-hidden">
 			<div className="grid mdsm:grid-cols-[40%_1fr] gap-x-8">
 				<div className="hidden mdsm:block mdsm:w-full mdsm:h-full">
 					<img src="./assets/history.png" alt="History picture" className="w-full h-full object-cover object-center"/>
 				</div>
 				<div className="py-8 px-8 mdsm:pl-0">
-					<h5>Info Session</h5>
-					<p className="text-body-1 -mt-1 mb-3 text-gray-500">Jun 8, 2023</p>
-					<p className="text-body-2">
+					<h3 className="text-fs-title-1 font-medium">Info Session</h3>
+					<p className="text-fs-body-1 -mt-1 mb-3 text-gray-500">Jun 8, 2023</p>
+					<p className="text-fs-body-2">
 						Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi
 						consectetur maxime fugit commodi quisquam voluptates eveniet
 						cupiditate tempora non ea ullam a distinctio doloribus consequuntur,
